dd159bb7c4c3a54d783516383d27341c510d7786
WoodsHo <WoodsHo@163.com>
2018-02-20 11:04:55 +0800

    modify the default color theme

--- app/src/main/java/com/woodsho/absoluteplan/SplashActivity.java
+++ app/src/main/java/com/woodsho/absoluteplan/SplashActivity.java
@@ -4,33 +4,35 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.widget.ImageView;
-
-import com.woodsho.absoluteplan.utils.StatusBarUtil;
 
 /**
  * Created by hewuzhao on 17/12/9.
  */
 
 public class SplashActivity extends Activity {
+    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-//        ImageView imageView = (ImageView) findViewById(R.id.splash_view);
-//        imageView.setImageResource(R.drawable.splash_view);
-//        StatusBarUtil statusBar = new StatusBarUtil(this);
-//        statusBar.setImmersionBar();
 
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
+        mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
-        }, 1500);
+        }, 1000);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mHandler != null) {
+            mHandler.removeCallbacksAndMessages(null);
+        }
     }
 }
